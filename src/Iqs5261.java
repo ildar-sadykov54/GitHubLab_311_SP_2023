9987986ee4b275c4b5db713609585c3c9bb2cb0d
iqs5261 <ildar.sadykov54@gmail.com>
2023-03-30 14:45:21 -0400

    This is my commit

---
+++ src/Iqs5261.java
@@ -0,0 +1,7 @@
+public class Iqs5261 {
+    // This is my new class for GitHub
+    public static void studentInfo(){
+        System.out.println("Name: Ildar Sadykov");
+        System.out.println();
+    }
+}
